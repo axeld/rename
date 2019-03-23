8239be75c4ef5ef692893e36319548f672a39f07
Axel Dörfler <axeld@pinc-software.de>
2019-03-23 23:30:19 +0100

    WIP
    - Preview is now rendered with color hints for the regex groups.

--- rename.cpp
+++ rename.cpp
@@ -8,7 +8,11 @@
 #include <Alert.h>
 #include <Catalog.h>
 #include <CardView.h>
+#include <ControlLook.h>
+#include <LayoutBuilder.h>
 #include <ListView.h>
+#include <ObjectList.h>
+#include <ScrollView.h>
 #include <String.h>
 #include <TextControl.h>
 
@@ -22,6 +26,7 @@
 #include <kernel/fs_info.h>
 #include <kernel/fs_attr.h>
 
+#include <regex.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -32,14 +37,100 @@ static const uint32 kMsgRenameSettings = 'pReS';
 static const uint32 kMsgUpdateReplace = 'upRe';
 static const uint32 kMsgUpdateSearch = 'upSe';
 
+static rgb_color kGroupColor[] = {
+	make_color(100, 200, 100),
+	make_color(200, 100, 200),
+	make_color(100, 200, 200),
+	make_color(150, 200, 150),
+	make_color(100, 250, 100),
+};
+static const uint32 kGroupColorCount = 5;
 
 #define B_TRANSLATION_CONTEXT "Rename"
+
+#define MAX_GROUPS 10
+#define MAX_MATCHES 20
+
+
+struct Group {
+	int32	index;
+	int32	start;
+	int32	end;
+
+	Group(int32 index, int32 start, int32 end)
+		:
+		index(index),
+		start(start),
+		end(end)
+	{
+	}
+};
+
+
+class RenameAction {
+public:
+	virtual						~RenameAction();
+
+	virtual	bool				AddGroups(BObjectList<Group>& groups,
+									const char* string) const = 0;
+	virtual BString				Rename(BObjectList<Group>& groups,
+									const char* string) const = 0;
+};
+
+
+class RegularExpressionRenameAction : public RenameAction {
+public:
+								RegularExpressionRenameAction();
+	virtual						~RegularExpressionRenameAction();
+
+			bool				SetPattern(const char* pattern);
+			void				SetReplace(const char* replace);
+
+	virtual	bool				AddGroups(BObjectList<Group>& groups,
+									const char* string) const;
+	virtual BString				Rename(BObjectList<Group>& groups,
+									const char* string) const;
+
+private:
+			regex_t				fCompiledPattern;
+			bool				fValidPattern;
+			BString				fReplace;
+};
+
+
+class PreviewItem : public BStringItem {
+public:
+								PreviewItem(const entry_ref& ref);
+
+			void				SetRenameAction(const RenameAction& action);
+			const char*			TargetName() const
+									{ return fTargetName.String(); }
+
+	virtual	void				DrawItem(BView* owner, BRect frame,
+									bool complete);
+
+private:
+			void				_DrawGroupedText(BView* owner, BRect frame,
+									float x, const BString& text,
+									const BObjectList<Group>& groups,
+									int32 first);
+			void				_DrawGroup(BView* owner, uint32 groupIndex,
+									BRect frame, float start, float end);
+
+private:
+			entry_ref			fRef;
+			BString				fTargetName;
+			BObjectList<Group>	fGroups;
+			BObjectList<Group>	fRenameGroups;
+};
 
 
 class RenameWindow : public BWindow {
 public:
 								RenameWindow(BRect rect);
 	virtual						~RenameWindow();
+
+			void				AddRef(const entry_ref& ref);
 
 	virtual	void				MessageReceived(BMessage* message);
 
@@ -56,7 +147,7 @@ private:
 extern const char* __progname;
 static const char* kProgramName = __progname;
 
-BRect gWindowFrame(150, 150, 200, 200);
+BRect gWindowFrame(150, 150, 500, 400);
 bool gRecursive = true;
 bool gFromShell = false;
 bool gVerbose = false;
@@ -177,19 +268,281 @@ handleDirectory(BEntry &entry, int32 level)
 }
 
 
+//	#pragma mark - RenameAction
+
+
+RenameAction::~RenameAction()
+{
+}
+
+
+//	#pragma mark - RegularExpressionRenameAction
+
+
+RegularExpressionRenameAction::RegularExpressionRenameAction()
+	:
+	fValidPattern(false)
+{
+}
+
+
+RegularExpressionRenameAction::~RegularExpressionRenameAction()
+{
+	if (fValidPattern)
+		regfree(&fCompiledPattern);
+}
+
+
+bool
+RegularExpressionRenameAction::SetPattern(const char* pattern)
+{
+	// TODO: add options for flags
+	int result = regcomp(&fCompiledPattern, pattern, REG_EXTENDED | REG_ICASE);
+	// TODO: show/report error!
+	fValidPattern = result == 0;
+
+	return fValidPattern;
+}
+
+void
+RegularExpressionRenameAction::SetReplace(const char* replace)
+{
+	fReplace = replace;
+}
+
+
+bool
+RegularExpressionRenameAction::AddGroups(BObjectList<Group>& groupList,
+	const char* string) const
+{
+	if (!fValidPattern)
+		return false;
+
+	regmatch_t groups[MAX_GROUPS];
+
+	if (regexec(&fCompiledPattern, string, MAX_GROUPS, groups, 0))
+		return false;
+
+puts(string);
+	for (int groupIndex = 0; groupIndex < MAX_GROUPS; groupIndex++) {
+		if (groups[groupIndex].rm_so == -1)
+			break;
+
+		groupList.AddItem(new Group(groupIndex, groups[groupIndex].rm_so,
+			groups[groupIndex].rm_eo));
+		printf("\t[%d] %d - %d\n", groupIndex, groups[groupIndex].rm_so, groups[groupIndex].rm_eo);
+	}
+
+	return true;
+}
+
+
+BString
+RegularExpressionRenameAction::Rename(BObjectList<Group>& groupList,
+	const char* string) const
+{
+	BString stringBuffer;
+	if (!fValidPattern)
+		return stringBuffer;
+
+	regmatch_t groups[MAX_GROUPS];
+	if (regexec(&fCompiledPattern, string, MAX_GROUPS, groups, 0))
+		return stringBuffer;
+
+puts(fReplace);
+	stringBuffer = fReplace;
+	char* buffer = stringBuffer.LockBuffer(B_FILE_NAME_LENGTH);
+	char* target = buffer;
+
+	for (; target[0] != '\0'; target++) {
+		if (target[0] == '\\' && target[1] > '0' && target[1] <= '9') {
+			// References a group
+			int32 groupIndex = target[1] - '0';
+			int startOffset = groups[groupIndex].rm_so;
+			int endOffset = groups[groupIndex].rm_eo;
+			int length = endOffset - startOffset;
+			if (startOffset < 0 || strlen(buffer) + length - 1
+					> B_FILE_NAME_LENGTH)
+				break;
+
+			memmove(target + length, target + 2, strlen(target) - 1);
+			memmove(target, string + startOffset, length);
+
+			startOffset = target - buffer;
+			groupList.AddItem(new Group(groupIndex, startOffset,
+				startOffset + length));
+printf("\t[%d] %d - %d\n", groupList.CountItems() - 1, startOffset, startOffset + length);
+			target += length - 1;
+		}
+	}
+
+	stringBuffer.UnlockBuffer();
+	return stringBuffer;
+}
+
+
+//	#pragma mark -
+
+
+PreviewItem::PreviewItem(const entry_ref& ref)
+	:
+	BStringItem(ref.name),
+	fRef(ref),
+	fGroups(10, true)
+{
+}
+
+
+void
+PreviewItem::SetRenameAction(const RenameAction& action)
+{
+	fGroups.MakeEmpty();
+	fRenameGroups.MakeEmpty();
+
+	action.AddGroups(fGroups, fRef.name);
+	fTargetName = action.Rename(fRenameGroups, fRef.name);
+}
+
+
+void
+PreviewItem::DrawItem(BView* owner, BRect frame, bool complete)
+{
+	rgb_color lowColor = owner->LowColor();
+	float width = owner->Bounds().Width() / 2.f;
+
+	if (IsSelected() || complete) {
+		rgb_color color;
+		if (IsSelected())
+			color = ui_color(B_LIST_SELECTED_BACKGROUND_COLOR);
+		else
+			color = owner->ViewColor();
+
+		owner->SetLowColor(color);
+		owner->FillRect(frame, B_SOLID_LOW);
+	} else
+		owner->SetLowColor(owner->ViewColor());
+
+	float x = frame.left + be_control_look->DefaultLabelSpacing();
+
+	if (fGroups.IsEmpty()) {
+		// Text does not match
+		owner->SetLowColor(200, 100, 100);
+		owner->FillRect(BRect(x, frame.top, x + owner->StringWidth(fRef.name),
+			frame.bottom), B_SOLID_LOW);
+
+		owner->MovePenTo(x, frame.top + BaselineOffset());
+		owner->DrawString(fRef.name);
+	} else {
+		// Text does match, fill groups in different colors
+		int startIndex = fGroups.CountItems() == 1 ? 0 : 1;
+		_DrawGroupedText(owner, frame, x, fRef.name, fGroups, startIndex);
+	}
+
+	if (fRenameGroups.IsEmpty()) {
+		owner->MovePenTo(x + width, frame.top + BaselineOffset());
+		owner->DrawString(fTargetName);
+	} else {
+		_DrawGroupedText(owner, frame, x + width, fTargetName,
+			fRenameGroups, 0);
+	}
+
+	owner->SetLowColor(lowColor);
+}
+
+
+void
+PreviewItem::_DrawGroupedText(BView* owner, BRect frame, float x,
+	const BString& text, const BObjectList<Group>& groups, int32 first)
+{
+	uint32 count = text.CountChars();
+	BPoint sizes[count];
+	be_plain_font->GetEscapements(text.String(), count, NULL, sizes);
+
+	float fontSize = be_plain_font->Size();
+	int32 byteIndex = 0;
+	bool inGroup = false;
+	uint32 groupIndex = first;
+	const Group* group = groups.ItemAt(groupIndex);
+	float startOffset = 0;
+	float endOffset = 0;
+
+	for (uint32 i = 0; i < count; i++) {
+		int32 characterBytes = text.CountBytes(i, 1);
+		float offset = endOffset;
+		endOffset += sizes[i].x * fontSize;
+
+		if (byteIndex == group->start) {
+			startOffset = offset;
+			inGroup = true;
+		}
+
+		byteIndex += characterBytes;
+
+		if (byteIndex == group->end) {
+			// End group
+			if (inGroup) {
+				_DrawGroup(owner, group->index, frame, x + startOffset,
+					x + endOffset);
+				inGroup = false;
+			}
+
+			// Select next group, if any
+			if (++groupIndex == (uint32)groups.CountItems())
+				break;
+			group = groups.ItemAt(groupIndex);
+		}
+	}
+	if (inGroup) {
+		// End last group
+		_DrawGroup(owner, group->index, frame, x + startOffset,
+			x + endOffset);
+	}
+
+	owner->MovePenTo(x, frame.top + BaselineOffset());
+	owner->DrawString(text.String());
+}
+
+
+void
+PreviewItem::_DrawGroup(BView* owner, uint32 groupIndex, BRect frame,
+	float start, float end)
+{
+	owner->SetLowColor(kGroupColor[groupIndex % kGroupColorCount]);
+	owner->FillRect(BRect(start, frame.top, end, frame.bottom), B_SOLID_LOW);
+}
+
+
 //	#pragma mark -
 
 
 RenameWindow::RenameWindow(BRect rect)
 	:
 	BWindow(rect, B_TRANSLATE("Rename files"), B_DOCUMENT_WINDOW,
-		B_AUTO_UPDATE_SIZE_LIMITS, B_ASYNCHRONOUS_CONTROLS)
+		B_AUTO_UPDATE_SIZE_LIMITS | B_ASYNCHRONOUS_CONTROLS)
 {
 	fPatternControl = new BTextControl("Regular expression", NULL, NULL);
 	fPatternControl->SetModificationMessage(new BMessage(kMsgUpdateSearch));
 
 	fRenameControl = new BTextControl("Replace with", NULL, NULL);
 	fRenameControl->SetModificationMessage(new BMessage(kMsgUpdateReplace));
+
+	fPreviewList = new BListView("preview", B_SINGLE_SELECTION_LIST,
+		B_FULL_UPDATE_ON_RESIZE | B_WILL_DRAW | B_FRAME_EVENTS | B_NAVIGABLE);
+
+	BLayoutBuilder::Group<>(this, B_VERTICAL, 0)
+		.AddGrid(0.f)
+			.SetInsets(B_USE_WINDOW_SPACING)
+			.Add(fPatternControl->CreateLabelLayoutItem(), 0, 0)
+			.Add(fPatternControl->CreateTextViewLayoutItem(), 1, 0)
+			.Add(fRenameControl->CreateLabelLayoutItem(), 0, 1)
+			.Add(fRenameControl->CreateTextViewLayoutItem(), 1, 1)
+		.End()
+		.AddGroup(B_VERTICAL)
+			.SetInsets(-2)
+			.Add(new BScrollView("scroller", fPreviewList, 0, true, true))
+		.End();
+
+	fPatternControl->MakeFocus(true);
 }
 
 
@@ -202,12 +555,28 @@ RenameWindow::~RenameWindow()
 
 
 void
+RenameWindow::AddRef(const entry_ref& ref)
+{
+	fPreviewList->AddItem(new PreviewItem(ref));
+}
+
+
+void
 RenameWindow::MessageReceived(BMessage* message)
 {
 	switch (message->what) {
 		case kMsgUpdateSearch:
 		case kMsgUpdateReplace:
-			puts("update!");
+			RegularExpressionRenameAction action;
+			action.SetPattern(fPatternControl->Text());
+			action.SetReplace(fRenameControl->Text());
+
+			for (int32 i = 0; i < fPreviewList->CountItems(); i++) {
+				PreviewItem* item = static_cast<PreviewItem*>(
+					fPreviewList->ItemAt(i));
+				item->SetRenameAction(action);
+			}
+			fPreviewList->Invalidate();
 			break;
 	}
 }
@@ -260,10 +629,35 @@ printUsage()
 }
 
 
+class RenameApplication : public BApplication {
+public:
+	RenameApplication()
+		:
+		BApplication("application/x-vnd.pinc.rename")
+	{
+	}
+
+	virtual void ReadyToRun()
+	{
+//		readSettings();
+		RenameWindow* window = new RenameWindow(gWindowFrame);
+
+		entry_ref ref;
+		get_ref_for_path("/boot/home/Desktop/test-124.jpg", &ref);
+		window->AddRef(ref);
+
+		window->Show();
+	}
+};
+
 int
 main(int argc, char** argv)
 {
-	BApplication app("application/x-vnd.pinc.rename");
+//	BApplication app("application/x-vnd.pinc.rename");
+	RenameApplication app;
+
+	app.Run();
+	return 0;
 
 	if (argc == 1) {
 		printUsage();
